38aa963cd36033af9329b4058329bbb6c31b27a2
helderPM <hmartins123@hotmail.com>
2024-09-10 15:36:06 +0200

    remove import

--- src/main/java/com/pmu2/exec/infrastrure/inbound/CourseEntity.java
+++ src/main/java/com/pmu2/exec/infrastrure/inbound/CourseEntity.java
@@ -3,9 +3,7 @@ package com.pmu2.exec.infrastrure.inbound;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @AllArgsConstructor
@@ -19,7 +17,6 @@ public class CourseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long courseId;
-
 
     @Column(name = "nom", nullable = false)
     private String name;
