5add7dbc169a0a90d7253c3dc69ede88c2f61a4b
helderPM <hmartins123@hotmail.com>
2024-09-10 17:10:35 +0200

    add test to partantController

--- src/test/java/com/pmu2/exec/ExecAppIntegrationTests.java
+++ src/test/java/com/pmu2/exec/ExecAppIntegrationTests.java
@@ -5,6 +5,7 @@ import com.pmu2.exec.infrastrure.inbound.CourseJpaRepository;
 import com.pmu2.exec.infrastrure.inbound.PartantEntity;
 import com.pmu2.exec.infrastrure.inbound.PartantJpaRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,6 +56,7 @@ class ExecAppIntegrationTests {
             "mysql:8.0-debian"
     );
 
+
     @BeforeEach
     void testSetUp() {
 
@@ -79,122 +81,236 @@ class ExecAppIntegrationTests {
                 LocalDate.of(2023, 7, 27));
 
         courseJpaRepository.saveAll(List.of(b1, b2, b3));
+
     }
 
     private List<PartantEntity> getListParticipantA() {
         PartantEntity p1 = new PartantEntity("Partant AA", 909);
         PartantEntity p2 = new PartantEntity("Partant AB", 809);
-        PartantEntity p3 = new PartantEntity("Partant AC", 709);
 
-        return List.of(p1, p2, p3);
+        return List.of(p1, p2);
     }
 
-    @Test
-    void testFindAll() {
+    @Nested
+    class CourseIntegrationTests {
+        @Test
+        void testFindAll() {
 
-        // find all Course and return List<CourseEntity>
-        ParameterizedTypeReference<List<CourseEntity>> typeRef = new ParameterizedTypeReference<>() {
-        };
-        ResponseEntity<List<CourseEntity>> response = restTemplate.exchange(
-                BASEURI + "/pmu/course",
-                HttpMethod.GET,
-                null,
-                typeRef
-        );
+            // find all Course and return List<CourseEntity>
+            ParameterizedTypeReference<List<CourseEntity>> typeRef = new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<List<CourseEntity>> response = restTemplate.exchange(
+                    BASEURI + "/pmu/course",
+                    HttpMethod.GET,
+                    null,
+                    typeRef
+            );
 
 
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(3, Objects.requireNonNull(response.getBody()).size());
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals(3, Objects.requireNonNull(response.getBody()).size());
+        }
+
+        @Test
+        void testCreate() {
+
+            // Create a new CourseEntity E
+            String courseName = "Course E";
+            CourseEntity newCourse = new CourseEntity(courseName, 9, LocalDate.parse("2023-09-14"));
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+            HttpEntity<CourseEntity> request = new HttpEntity<>(newCourse, headers);
+
+            // test POST save
+            ResponseEntity<CourseEntity> responseEntity =
+                    restTemplate.postForEntity(BASEURI + "/pmu/course", request, CourseEntity.class);
+
+            assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+            // find Course E
+            List<CourseEntity> list = courseJpaRepository.findByName(courseName);
+
+            // Test Course E details
+            CourseEntity course = list.getFirst();
+            assertEquals(courseName, course.getName());
+            assertEquals(9, course.getNumber());
+            assertEquals(LocalDate.of(2023, 9, 14), course.getDate());
+
+        }
+
+        @Test
+        void testFindByName() {
+            String courseName = "Course A";
+            List<CourseEntity> list = courseJpaRepository.findByName(courseName);
+            CourseEntity courseEntityA = list.isEmpty() ? null : list.getFirst();
+
+            ParameterizedTypeReference<List<CourseEntity>> typeRef = new ParameterizedTypeReference<>() {
+            };
+
+            // find Course A
+            ResponseEntity<List<CourseEntity>> response = restTemplate.exchange(
+                    BASEURI + "/pmu/course/find/" + courseName,
+                    HttpMethod.GET,
+                    null,
+                    typeRef
+            );
+
+            // test response code
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+
+            List<CourseEntity> listResponse = response.getBody();
+            assert listResponse != null;
+
+            assertEquals(1, listResponse.size());
+
+            // Test Course A details
+            CourseEntity courseEntity = list.getFirst();
+            assertEquals(courseName, courseEntity.getName());
+            assertEquals(99, courseEntity.getNumber());
+            assertEquals(LocalDate.of(2023, 8, 28), courseEntity.getDate());
+
+            // Compare PartantEntity objects
+            assert courseEntityA != null;
+            assertEquals(courseEntityA.getPartants().size(), listResponse.getFirst().getPartants().size());
+
+        }
+
+        @Test
+        void testDeleteById() {
+
+            String courseName = "Course C";
+            List<CourseEntity> list = courseJpaRepository.findByName(courseName);
+            CourseEntity courseEntityA = list.getFirst();
+
+            // get course A id
+            Long id = courseEntityA.getCourseId();
+
+            // delete by id
+            ResponseEntity<Void> response = restTemplate.exchange(
+                    BASEURI + "/pmu/course/" + id,
+                    HttpMethod.DELETE,
+                    null,
+                    Void.class
+            );
+
+            // test 204
+            assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+            // find Course A again, ensure no result
+            List<CourseEntity> listAgain = courseJpaRepository.findByName(courseName);
+            assertEquals(0, listAgain.size());
+
+        }
+
     }
 
-    @Test
-    void testCreate() {
+    @Nested
+    class PartantIntegrationTests {
+        @Test
+        void testPartantFindAll() {
 
-        // Create a new CourseEntity E
-        String courseName = "Course E";
-        CourseEntity newBook = new CourseEntity(courseName, 9, LocalDate.parse("2023-09-14"));
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        HttpEntity<CourseEntity> request = new HttpEntity<>(newBook, headers);
+            // find all Partant and return List<PartantEntity>
+            ParameterizedTypeReference<List<PartantEntity>> typeRef =
+                    new ParameterizedTypeReference<>() {
+            };
+            ResponseEntity<List<PartantEntity>> response = restTemplate.exchange(
+                    BASEURI + "/pmu/partant",
+                    HttpMethod.GET,
+                    null,
+                    typeRef
+            );
 
-        // test POST save
-        ResponseEntity<CourseEntity> responseEntity =
-                restTemplate.postForEntity(BASEURI + "/pmu/course", request, CourseEntity.class);
+            assertEquals(HttpStatus.OK, response.getStatusCode());
+            assertEquals(2, Objects.requireNonNull(response.getBody()).size());
+        }
 
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        @Test
+        void testPartantCreate() {
 
-        // find Course E
-        List<CourseEntity> list = courseJpaRepository.findByName(courseName);
+            // Create a new CourseEntity E
+            String partantName = "Partant EE";
+            PartantEntity newPartant = new PartantEntity(partantName, 14);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", "application/json");
+            HttpEntity<PartantEntity> request = new HttpEntity<>(newPartant, headers);
 
-        // Test Course E details
-        CourseEntity course = list.getFirst();
-        assertEquals(courseName, course.getName());
-        assertEquals(9, course.getNumber());
-        assertEquals(LocalDate.of(2023, 9, 14), course.getDate());
+            // test POST save
+            ResponseEntity<PartantEntity> responseEntity =
+                    restTemplate.postForEntity(BASEURI + "/pmu/partant", request, PartantEntity.class);
 
-    }
+            assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
 
-    @Test
-    void testFindByName() {
-        String courseName = "Course A";
-        List<CourseEntity> list = courseJpaRepository.findByName(courseName);
-        CourseEntity courseEntityA = list.isEmpty() ? null : list.getFirst();
+            // find Course E
+            List<PartantEntity> list = partantJpaRepository.findByName(partantName);
 
-        ParameterizedTypeReference<List<CourseEntity>> typeRef = new ParameterizedTypeReference<>() {
-        };
+            // Test Course E details
+            PartantEntity partant1 = list.getFirst();
+            assertEquals(partantName, partant1.getName());
+            assertEquals(14, partant1.getNumber());
 
-        // find Course A
-        ResponseEntity<List<CourseEntity>> response = restTemplate.exchange(
-                BASEURI + "/pmu/course/find/" + courseName,
-                HttpMethod.GET,
-                null,
-                typeRef
-        );
+        }
 
-        // test response code
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        @Test
+        void testPartantFindByName() {
+            String partantName = "Partant AA";
+            List<PartantEntity> list = partantJpaRepository.findByName(partantName);
 
-        List<CourseEntity> listResponse = response.getBody();
-        assert listResponse != null;
+            ParameterizedTypeReference<List<PartantEntity>> typeRef = new ParameterizedTypeReference<>() {
+            };
 
-        assertEquals(1, listResponse.size());
+            // find partant AA
+            ResponseEntity<List<PartantEntity>> response = restTemplate.exchange(
+                    BASEURI + "/pmu/partant/find/" + partantName,
+                    HttpMethod.GET,
+                    null,
+                    typeRef
+            );
 
-        // Test Course A details
-        CourseEntity courseEntity = list.getFirst();
-        assertEquals(courseName, courseEntity.getName());
-        assertEquals(99, courseEntity.getNumber());
-        assertEquals(LocalDate.of(2023, 8, 28), courseEntity.getDate());
+            // test response code
+            assertEquals(HttpStatus.OK, response.getStatusCode());
 
-        // Compare PartantEntity objects
-        assertEquals(courseEntityA.getPartants().size(), listResponse.getFirst().getPartants().size());
+            List<PartantEntity> listResponse = response.getBody();
+            assert listResponse != null;
 
-    }
+            assertEquals(1, listResponse.size());
 
-    @Test
-    void testDeleteById() {
+            // Test partant A details
+            PartantEntity partantEntity = list.getFirst();
+            assertEquals(partantName, partantEntity.getName());
+            assertEquals(909, partantEntity.getNumber());
 
-        String courseName = "Course C";
-        List<CourseEntity> list = courseJpaRepository.findByName(courseName);
-        CourseEntity courseEntityA = list.getFirst();
+        }
 
-        // get course A id
-        Long id = courseEntityA.getCourseId();
+        @Test
+        void testPartantDeleteById() {
 
-        // delete by id
-        ResponseEntity<Void> response = restTemplate.exchange(
-                BASEURI + "/pmu/course/" + id,
-                HttpMethod.DELETE,
-                null,
-                Void.class
-        );
+            String partantName = "Partant AC";
+            PartantEntity p3 = new PartantEntity(partantName, 709);
+            var tt = partantJpaRepository.save(p3);
 
-        // test 204
-        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+            List<PartantEntity> list = partantJpaRepository.findByName(partantName);
+            PartantEntity partantEntityA = list.getFirst();
 
-        // find Book A again, ensure no result
-        List<CourseEntity> listAgain = courseJpaRepository.findByName(courseName);
-        assertEquals(0, listAgain.size());
+            // get partant A id
+            Long id = partantEntityA.getId();
+
+            // delete by id
+            ResponseEntity<Void> response = restTemplate.exchange(
+                    BASEURI + "/pmu/partant/" + id,
+                    HttpMethod.DELETE,
+                    null,
+                    Void.class
+            );
+
+            // test 204
+            assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+            // find partant A again, ensure no result
+            List<PartantEntity> listAgain = partantJpaRepository.findByName(partantName);
+            assertEquals(0, listAgain.size());
+
+        }
 
     }
 
